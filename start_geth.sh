022565f61e32fa0aa097e3da8a756eb7ead72883
Max Weng <max.m.weng@gmail.com>
2016-08-05 18:13:36 +0800

    Fixed the data dir path for creating new account. Added personal plugin for unlocking accounts.

--- start_geth.sh
+++ start_geth.sh
@@ -1,5 +1,5 @@
 #!/bin/ash
-addr="$(./bin/geth --password secret.txt account new | sed -e 's/[^{]*{\([^}]*\)}.*/\1/g')"
+addr="$(./bin/geth --datadir ./data/ --password secret.txt account new | sed -e 's/[^{]*{\([^}]*\)}.*/\1/g')"
 echo "New coinbase address: $addr"
 ./bin/geth --datadir ./data/ init genesis.json
-./bin/geth --datadir ./data/ --rpc --rpcaddr 0.0.0.0 --rpccorsdomain '*' --rpcapi 'web3,eth,debug,net,ssh' --ipcpath /run/geth.ipc --mine --minerthreads 1 --etherbase $addr
+./bin/geth --datadir ./data/ --rpc --rpcaddr 0.0.0.0 --rpccorsdomain '*' --rpcapi 'web3,eth,debug,net,ssh,personal' --ipcpath /run/geth.ipc --mine --minerthreads 1 --etherbase $addr
